18f7634b9ff3fb2eeb5b78171fcd2000ec0aeb04
Shidhin C R <shidhincr@gmail.com>
2016-02-10 23:14:28 +0400

    Adding the dummy JSON and renderinf the list from that

--- app/app.js
+++ app/app.js
@@ -2,23 +2,49 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ProgressBar from 'react-toolbox/lib/progress_bar';
 import  Header from './components/header';
 import  Search from './components/search';
 import ContributeButton from './components/contribute';
 import LinksList from './components/lists';
+import styles from './css/styles.scss';
 import 'normalize.css';
 
 let {Component}  = React;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      listData: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch('./links.json').then(res => res.json()).then(listData => {
+      this.setState({
+        loading: false,
+        listData: listData
+      });
+    });
+  }
+
   render() {
     return (
       <div>
         <Header title="Awesome ES6"/>
         <Search/>
-        <div style={{padding: '20px'}}>
-          <LinksList />
+        { this.state.loading &&
+        <div className={styles.absCenter}>
+          <ProgressBar type="circular" mode="indeterminate"/>
         </div>
+        }
+        { Object.keys(this.state.listData).length > 0 &&
+        <div style={{padding: '20px'}}>
+          <LinksList data={this.state.listData}/>
+        </div>
+        }
         <ContributeButton/>
       </div>
     );
